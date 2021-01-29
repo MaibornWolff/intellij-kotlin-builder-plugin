c47d885e0ec7ecddc8a564ea5cd349a3f15685f7
OemerA <oemer.ayar@maibornwolff.de>
2021-01-29 10:54:43 +0100

    check whether element under cursor is class or not

--- src/main/kotlin/actions/GenerateBuilderAction.kt
+++ src/main/kotlin/actions/GenerateBuilderAction.kt
@@ -3,14 +3,27 @@ package actions
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.ui.Messages
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
 
 class GenerateBuilderAction: AnAction() {
 
     override fun actionPerformed(event: AnActionEvent) {
-        Messages.showMessageDialog(event.project,
-                                   "Dialog message",
-                                   "Dialog Title",
-                                   Messages.getInformationIcon());
+
+        val element = event.dataContext.getData("psi.Element") as? PsiClass
+        if (element == null) {
+            Messages.showMessageDialog(event.project,
+                                       "Bitte nur in Klassen verwenden",
+                                       "Fehler",
+                                       Messages.getInformationIcon())
+        } else {
+            Messages.showMessageDialog(event.project,
+                                       element.name,
+                                       "Dialog Title",
+                                       Messages.getInformationIcon())
+        }
+
     }
 
 }
